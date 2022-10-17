8c41fe63a6b21a4b14b7094131fa4f8dabe0fb45
Leeseul-Moon <leeseulmoon4@gmail.com>
2022-10-17 10:07:24 +0900

    Feat: dialog

--- src/components/page/page.ts
+++ src/components/page/page.ts
@@ -5,8 +5,15 @@ export interface Composable {
 }
 
 type OnCloseListener = () => void;
+type SectionContainerConstructor = {
+  new (): SectionContainer;
+};
 
-class PageItemComponent extends BaseComponent<HTMLElement> implements Composable {
+interface SectionContainer extends Component, Composable {
+  setOnCloseListener(listener: OnCloseListener): void;
+}
+
+export class PageItemComponent extends BaseComponent<HTMLElement> implements SectionContainer {
   private closeListener?: OnCloseListener;
   constructor() {
     super(`<li class="page-item">
@@ -30,11 +37,11 @@ class PageItemComponent extends BaseComponent<HTMLElement> implements Composable
 }
 
 export class PageComponent extends BaseComponent<HTMLUListElement> implements Composable {
-  constructor() {
+  constructor(private pageItemConstructor: SectionContainerConstructor) {
     super(`<ul class="page"></ul>`);
   }
   addChild(section: Component) {
-    const item = new PageItemComponent();
+    const item = new this.pageItemConstructor();
     item.addChild(section);
     item.attachTo(this.element, "beforeend");
     item.setOnCloseListener(() => {
